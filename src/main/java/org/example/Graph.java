bf82e256607ff5a4f2ba24164419a4a21b74f755
dreik84 <dreik84@mail.ru>
2025-11-19 16:18:32 +0300

    Update Graph.java, add dfs method

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -5,11 +5,13 @@ public class Graph {
     private Vertex vertexList[];
     private int adjMat[][];
     private int nVerts;
+    private StackX theStack;
 
     public Graph() {
         vertexList = new Vertex[MAX_VERTS];
         adjMat = new int[MAX_VERTS][MAX_VERTS];
         nVerts = 0;
+        theStack = new StackX();
 
         for (int j = 0; j < MAX_VERTS; j++)
             for (int k = 0; k < MAX_VERTS; k++)
@@ -27,5 +29,37 @@ public class Graph {
 
     public void displayVertex(int v) {
         System.out.print(vertexList[v].label);
+    }
+
+    // Depth First Search
+    public void dfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theStack.push(0);
+
+        while (!theStack.isEmpty()) {
+            int v = getAdjUnvisitedVertex(theStack.peek());
+
+            if (v == -1) {
+                theStack.pop();
+            } else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                theStack.push(v);
+            }
+        }
+
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].wasVisited = false;
+        }
+    }
+
+    public int getAdjUnvisitedVertex(int v) {
+
+        for (int i = 0; i < nVerts; i++)
+            if (adjMat[v][i] == 1 && vertexList[i].wasVisited == false)
+                return i;
+
+        return -1;
     }
 }

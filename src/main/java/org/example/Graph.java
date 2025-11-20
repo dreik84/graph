581f436b0d4a93ffbe49904c68f5bc390a81a9f1
dreik84 <dreik84@mail.ru>
2025-11-20 16:17:33 +0300

    Update Graph.java, add bfs method

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -6,12 +6,14 @@ public class Graph {
     private int adjMat[][];
     private int nVerts;
     private StackX theStack;
+    private Queue theQueue;
 
     public Graph() {
         vertexList = new Vertex[MAX_VERTS];
         adjMat = new int[MAX_VERTS][MAX_VERTS];
         nVerts = 0;
         theStack = new StackX();
+        theQueue = new Queue();
 
         for (int j = 0; j < MAX_VERTS; j++)
             for (int k = 0; k < MAX_VERTS; k++)
@@ -46,6 +48,28 @@ public class Graph {
                 vertexList[v].wasVisited = true;
                 displayVertex(v);
                 theStack.push(v);
+            }
+        }
+
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].wasVisited = false;
+        }
+    }
+
+    // Breadth First Search
+    public void bfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theQueue.insert(0);
+        int v2;
+
+        while (!theQueue.isEmpty()) {
+            int v1 = theQueue.remove();
+
+            while ((v2 = getAdjUnvisitedVertex(v1)) != -1) {
+                vertexList[v2].wasVisited = true;
+                displayVertex(v2);
+                theQueue.insert(v2);
             }
         }
 

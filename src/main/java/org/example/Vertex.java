684c4be25c02316650fdebee261651676a99a857
dreik84 <dreik84@mail.ru>
2025-11-26 16:14:32 +0300

    Update Vertex.java

--- src/main/java/org/example/Vertex.java
+++ src/main/java/org/example/Vertex.java
@@ -3,9 +3,11 @@ package org.example;
 public class Vertex {
     public char label;
     public boolean wasVisited;
+    public boolean isInTree;
 
     public Vertex(char lab) {
         label = lab;
         wasVisited = false;
+        isInTree = false;
     }
 }
